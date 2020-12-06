6b1b45f541b4de8274b0e042ec353cae8151115b
DhanushkumarSivaji <dhanushkumar.s@yahoo.com>
2020-12-06 23:59:12 +0530

    complete authenticating in microfrontends

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({onSignIn}) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -14,6 +14,10 @@ export default () => {
         if (pathname !== nextPathname) {
           history.push(nextPathname);
         }
+      },
+      onSignIn: () => {
+        console.log("i am called")
+        onSignIn()
       }
     });
     history.listen(onParentNavigate)
