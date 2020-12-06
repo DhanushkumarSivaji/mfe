7d17f8124e106f7fab14c486e40a745e7d384ed9
DhanushkumarSivaji <dhanushkumar.s@yahoo.com>
2020-12-06 23:36:06 +0530

    complete performance consideration

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import SignIn from './components/Signin';
+import SignUp from './components/Signup';
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'ma'
+  productionPrefix: 'au'
 })
 
 
@@ -15,8 +15,8 @@ export default ({ history }) => {
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
-            <Route path="/pricing" component={Pricing} />
-            <Route path="/" component={Landing} />
+            <Route exact path="/auth/signin" component={SignIn}/>
+            <Route exact path="/auth/signup" component={SignUp}/>
           </Switch>
         </Router >
       </StylesProvider>
